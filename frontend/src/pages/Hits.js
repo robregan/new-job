fa799fd0ec1f2484226d151e0597f60d622a7f7d
rob <robert_regan86@yahoo.com>
2022-02-27 13:25:33 -0500

    got the form data to display on the hits page

--- frontend/src/pages/Hits.js
+++ frontend/src/pages/Hits.js
@@ -5,13 +5,25 @@ import {useHits} from '../context/Hits'
 const Hits = () => {
  const hits = useHits()
 
-  console.log(hits)
 
+  console.log(hits)
+  console.log(hits[0].companyName)
   return (
 
     
     <div className="container">
-   <p>  {hits} </p>
+      <h1> My current hits </h1>
+      
+    { hits.map((hit, i )=> (
+      <ul>
+        <li>Company Name: {hit.companyName} </li>
+        <li>Contact Email: {hit.contactEmail} </li>
+        <li>Job Title: {hit.jobTitle} </li>
+        <li>Company Website {hit.companySite} </li>
+        <li>Have you applied?  {hit.appliedYes ? "Yes" : "No"} </li>
+      </ul>
+    )) }
+    
     </div>
   )
 }
